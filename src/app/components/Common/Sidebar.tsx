731cdbd41b41bb2f9e8796642e37428890c88355
NishigandhaGawas <129956160+NishigandhaGawas@users.noreply.github.com>
2024-06-11 21:39:15 +0530

    feat: side bar changes

--- src/app/components/Common/Sidebar.tsx
+++ src/app/components/Common/Sidebar.tsx
@@ -1,5 +1,13 @@
 "use client";
-import { Box, VStack, Text, Link, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  VStack,
+  Text,
+  Link,
+  Flex,
+  Divider,
+  HStack,
+} from "@chakra-ui/react";
 import Logo from "../Icons/Logo";
 import {
   NavDashboard,
@@ -8,6 +16,9 @@ import {
   NavLogout,
   NavEditProfile,
   NavDocuments,
+  NavTeams,
+  NavChapters,
+  NavOrganization,
 } from "../Icons";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
@@ -39,6 +50,44 @@ const Sidebar = () => {
         />
       ),
       href: "/company",
+      options: [
+        {
+          label: "Teams",
+          icon: (
+            <NavTeams
+              h="18px"
+              w="18px"
+              mr="8px"
+              color={pathname === "/dashboard" ? "#fff" : "#2C365C"}
+            />
+          ),
+          href: "/company/teams",
+        },
+        {
+          label: "Chapters",
+          icon: (
+            <NavChapters
+              h="18px"
+              w="18px"
+              mr="8px"
+              color={pathname === "/dashboard" ? "#fff" : "#2C365C"}
+            />
+          ),
+          href: "/company/chapters",
+        },
+        {
+          label: "Organization",
+          icon: (
+            <NavOrganization
+              h="18px"
+              w="18px"
+              mr="8px"
+              color={pathname === "/dashboard" ? "#fff" : "#2C365C"}
+            />
+          ),
+          href: "/company/organization",
+        },
+      ],
     },
     {
       label: "Time Sheet",
@@ -107,27 +156,79 @@ const Sidebar = () => {
       <VStack as="nav">
         {siderBarItem.map((item, i) => {
           return (
-            <Link
-              href={item.href}
-              bg={pathname === item.href ? "#3BCBBE" : "#fff"}
-              color={
-                pathname === item.href
-                  ? "#fff"
-                  : item.label === "Logout"
-                  ? "#FF3535"
-                  : "#2C365C"
-              }
-              w="100%"
-              h="50px"
-              borderRadius="8px"
-              p="4"
-              key={i}
-            >
-              <Flex>
-                {item.icon}
-                <Text> {item.label}</Text>
-              </Flex>
-            </Link>
+            <>
+              <Link
+                href={item.href}
+                bg={pathname === item.href ? "#3BCBBE" : "#fff"}
+                color={
+                  pathname === item.href
+                    ? "#fff"
+                    : item.label === "Logout"
+                    ? "#FF3535"
+                    : "#2C365C"
+                }
+                w="100%"
+                h="50px"
+                borderRadius="8px"
+                p="4"
+                key={i}
+              >
+                <Flex>
+                  {item.icon}
+                  <Text> {item.label}</Text>
+                </Flex>
+              </Link>
+              {pathname === "/company" && item.href === "/company" && (
+                <HStack gap={0}>
+                  <Divider
+                    orientation="vertical"
+                    color="#80869D"
+                    borderColor="#80869D"
+                    h="133px"
+                    ms={4}
+                  />
+                  <VStack gap={0} alignItems="flex-start">
+                    {item.options?.map((item, i) => {
+                      return (
+                        <HStack>
+                          <Divider
+                            orientation="horizontal"
+                            color="#80869D"
+                            borderColor="#80869D"
+                            w="10px"
+                          />
+                          <Link
+                            href={item.href}
+                            bg={
+                              item.href === "/company/teams"
+                                ? "#F7F8FA"
+                                : "#fff"
+                            }
+                            color={
+                              pathname === item.href
+                                ? "#fff"
+                                : item.label === "Logout"
+                                ? "#FF3535"
+                                : "#2C365C"
+                            }
+                            w="100%"
+                            h="50px"
+                            borderRadius="8px"
+                            p="4"
+                            key={i}
+                          >
+                            <Flex>
+                              {item.icon}
+                              <Text> {item.label}</Text>
+                            </Flex>
+                          </Link>
+                        </HStack>
+                      );
+                    })}
+                  </VStack>
+                </HStack>
+              )}
+            </>
           );
         })}
       </VStack>
