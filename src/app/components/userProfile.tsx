50a1df5b7227d71e9e0ba98aed161ab3cf3f44f7
Nishigandha Gawas <gawasnishigandha@gmail.com>
2024-06-13 20:24:17 +0530

    feat: dashboard integration (#19)
    Co-authored-by: NishigandhaGawas <129956160+NishigandhaGawas@users.noreply.github.com>

--- src/app/components/userProfile.tsx
+++ src/app/components/userProfile.tsx
@@ -1,13 +1,16 @@
+"use client";
 import React from "react";
 import { Avatar, Box, Flex, Image, Text } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
 
 export default function UserProfile() {
+  const userData = useSession();
   return (
     <Flex alignItems="center">
       <Avatar size="sm" name="jessie" src="./images/peoples/jessie.svg" />
       <Box ms="2">
         <Text fontSize="sm" color="brand.800" fontWeight="normal">
-          Mohamad Azaruddin
+          {userData.data?.user?.name}
         </Text>
         <Text fontSize="xs" fontWeight="normal" color="#00000099">
           UI Developer
--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -2,3 +2,4 @@ export { MyHTTPClient } from "./httpClient/myHttpClient";
 export { GetData } from "./timesheet/getData";
 export { PostTaskData } from "./timesheet/postData";
 export { GetOptions } from "./timesheet/getOptions";
+export { GetDashboardCount } from "./dashboard/getDashboardCount";
