c6f3f6a5cb89c36913e6217ddf150a4c833e2408
NishigandhaGawas <129956160+NishigandhaGawas@users.noreply.github.com>
2024-06-14 20:08:48 +0530

    feat: documents

--- src/app/(pages)/documents/page.tsx
+++ src/app/(pages)/documents/page.tsx
@@ -12,6 +12,20 @@ import { XlsxIcon, MenuIcon } from "@/app/components/Icons";
 import { DocumentCard } from "@/app/components";
 
 export default function Documents() {
+  const documentData = [
+    {
+      name: "HR_Policies.pdf",
+      size: "2 MB",
+      updated: "04/05/2023",
+      url: "/documents/HR_Policy.pdf",
+    },
+    {
+      name: "Holiday_list.pdf",
+      size: "3 MB",
+      updated: "15/03/2023",
+      url: "/documents/Holiday_list.pdf",
+    },
+  ];
   return (
     <VStack w="100%" p={6} alignItems={"flex-start"}>
       <HStack
@@ -22,12 +36,29 @@ export default function Documents() {
         borderRadius="8px"
       ></HStack>
       <Grid templateColumns="repeat(5, 1fr)" mt={10}>
-        <GridItem colSpan={1} bg="#fff" p={4} borderRadius="8px" mr={10}>
-          <DocumentCard bg="#fff" p={4} borderRadius="8px" mr={10} />
-        </GridItem>
-        <GridItem colSpan={1} bg="#fff" p={4} borderRadius="8px" mr={10}>
-          <DocumentCard bg="#fff" p={4} borderRadius="8px" mr={10} />
-        </GridItem>
+        {documentData.map((document, i) => {
+          return (
+            <GridItem colSpan={1} bg="#fff" p={4} borderRadius="8px" mr={10}>
+              <DocumentCard
+                bg="#fff"
+                p={4}
+                borderRadius="8px"
+                mr={10}
+                documentData={document}
+              />
+            </GridItem>
+          );
+        })}
+
+        {/* <GridItem colSpan={1} bg="#fff" p={4} borderRadius="8px" mr={10}>
+          <DocumentCard
+            bg="#fff"
+            p={4}
+            borderRadius="8px"
+            mr={10}
+            documentData={documentData}
+          />
+        </GridItem> */}
       </Grid>
     </VStack>
   );
